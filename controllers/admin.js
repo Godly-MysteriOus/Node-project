b38f5097e2ab74a51c7b65ca533f87e7f3803f72
Godly-MysteriOus <singhrajputjayant8@gmail.com>
2024-08-06 00:58:56 +0530

    edit and delete buttons are now live in admin products + some minor bug fixes ehich occured when item got deleted from the cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,12 +1,13 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    editing:false
+    // formsCSS: true,
+    // productCSS: true,
+    // activeAddProduct: true
   });
 };
 
@@ -15,10 +16,51 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({title:title,price:price,imageUrl:imageUrl,description:description}).then().catch(err=>console.log(err));
-  res.redirect('/');
+  Product.create({title:title,price:price,imageUrl:imageUrl,description:description}).then(result=> res.redirect('/')).catch(err=>console.log(err));
+  
 };
+exports.getEditProduct = (req,res,next)=>{
+  const prodId = req.params.productID;
+  const editMode = req.query.edit;
 
+  Product.findOne({where:{id:prodId}}).then(product=>{
+      res.render('admin/edit-product',{
+        pageTitle: 'Edit Product',
+        path:'/admin/edit-product',
+        product:product,
+        editing:editMode
+      })
+  });
+}
+exports.postEditProduct = (req,res,next)=>{
+  const prodId = req.body.productID;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+
+  Product.findOne({where:{id:prodId}}).then(async product=>{
+    product.title = updatedTitle;
+    product.imageUrl = updatedImageUrl;
+    product.price = updatedPrice;
+    product.description = updatedDescription;
+    await product.save();
+  })
+  .then(result=>{
+    console.log('UPDATED PRODUCT SUCCESSFULLY');
+    res.redirect('/admin/products');
+  })
+  .catch(err=>console.log(err));
+}
+exports.postDeleteProduct = (req,res,next)=>{
+  const prodId = req.body.prodID;
+  console.log(prodId);
+  Product.findOne({where:{id:prodId}})
+  .then(result=>{
+    Product.destroy({where:{id:prodId}});
+    res.redirect('/admin/products');
+  });
+}
 exports.getProducts = (req, res, next) => {
  Product.findAll().then(products=>{
   res.render('admin/products', {
